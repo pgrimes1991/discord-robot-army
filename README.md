73bdd4a276a660141daafc32f117256ec178d721
Patrick Grimes <pgrimes1991@yahoo.com>
2020-11-11 23:22:40 -0500

    Add election bot

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Repo for my army of discord bots
+
+Leverages discordpy library https://discordpy.readthedocs.io/en/latest/
--- counter-bot/simp.py
+++ counter-bot/simp.py
@@ -2,6 +2,59 @@ import discord
 from datetime import datetime
 import pytz
 import config
+import requests
+
+import re
+import pdb
+import urllib
+import requests
+from bs4 import BeautifulSoup
+
+def get_election(state=None):
+    query = "election results {state}".format(state=state)
+    query = query.replace(' ', '+')
+    URL = f"https://google.com/search?q={query}"
+    # desktop user-agent
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+    # mobile user-agent
+    MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+    headers = {"user-agent" : MOBILE_USER_AGENT}
+    resp = requests.get(URL, headers=headers)
+    p = re.compile(r'%')
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.content, "html.parser")
+
+    results = []
+    print(len(soup.find_all('div')), 'DIVLEN')
+    for idx, g in enumerate(soup.find_all('div')):
+        if 'Donald Trump' in g.text and idx>70:
+            print(idx, 'TRUMP')
+            name = 'Trump'
+            break
+        if 'Joe Biden' in g.text and idx>70:
+            print(idx, 'BIDEN')
+            name = 'Biden'
+            break
+    for g in soup.find_all('span'):
+        if 'reporting' in g.text:
+            print(g.text[0:4])
+            reporting = g.text[0:4]
+            break
+    i=-1
+    for g in soup.find_all('span'):
+        if 'Vote count' in g.text:
+            i=1
+        if i>=1:
+            if g.text.strip()!='':
+                results.append(g.text)
+            print(g.text)
+            i+=1
+
+        if len(results)==5:
+            i=-1
+
+    return "{name} lead {lead} \n Percent reporting {reporting}".format(name=name, reporting=reporting, lead=int(results[2].replace(',',''))-int(results[4].replace(',','')))
+
 
 client = discord.Client()
 
@@ -17,6 +70,18 @@ local = pytz.timezone ("US/Eastern")
 
 @client.event
 async def on_message(message):
+    if message.content.startswith('!PA'):
+        election_message = get_elelection_message = get_election('PA')
+        await message.channel.send(election_message)
+    if message.content.startswith('!GA'):
+        election_message = get_elelection_message = get_election('GA')
+        await message.channel.send(election_message)
+    if message.content.startswith('!AZ'):
+        election_message = get_elelection_message = get_election('AZ')
+        await message.channel.send(election_message)
+    if message.content.startswith('!NC'):
+        election_message = get_elelection_message = get_election('NC')
+        await message.channel.send(election_message)
     if message.content.startswith('!countstonks'):
         limit = 5000
         if ' ' in message.content:
